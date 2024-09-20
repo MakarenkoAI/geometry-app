8993a14679ee4463f814e351ba1cf39d5c84d4cb
Svyatoslav Silibin <silibin@hiddenteam.ru>
2024-09-20 18:42:00 +0300

    Change structure

---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/editor">Editor</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/editor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Editor() {
+  return (
+    <div>
+      <h1>This is the editor page</h1>
+      <p>Welcome to the editor page!</p>
+    </div>
+  );
+};
+
+export default Editor;
