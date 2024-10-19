874817010b1b57dc0c4e68e0f8b299a3dc857627
fekron <r.vorontsov03@gmail.com>
2024-10-19 18:29:24 +0300

    add introduction window

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/home';
 import Editor from './pages/Editor/editor';
 import Chat from './pages/Chat/chat';
+import Intro from './pages/Intro/intro';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />  {/* Главная страница */}
         <Route path="/editor" element={<Editor />} />  {/* Редактор */}
         <Route path="/chat" element={<Chat />} />  {/* Чат */}
+        <Route path="/intro" element={<Intro />} />  {/* Окно знакомства с пользователем */}
       </Routes>
     </Router>
   );
